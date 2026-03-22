bdf34189b85178d631ce43cd2683eeb19c30a8bd
chawanprakash421-cyber <chawanprakash421@gmail.com>
2026-03-22 12:42:08 +0530

    main

--- leads.js
+++ leads.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('../db/supabase');
+const supabase = require('./db/supabase');
 const { authMiddleware } = require('./auth');
 
 router.use(authMiddleware);
