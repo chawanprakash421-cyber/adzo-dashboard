a1a1ddba92f1bd013dd15d897f596f92e6fe93d8
chawanprakash421-cyber <chawanprakash421@gmail.com>
2026-03-22 12:36:06 +0530

    main

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const supabase = require('../db/supabase');
+const supabase = require('./db/supabase');
 
 function signToken(user) {
   return jwt.sign(
